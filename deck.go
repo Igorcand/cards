bce4de6c3eea4e6373cfcdbcbb0dd27d9aa1dfd4
Igor Candido Rodrigues <87720535+Igorcand@users.noreply.github.com>
2023-11-22 08:19:48 -0300

    Saving data to a hard drive

--- deck.go
+++ deck.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
+// Create a new type of "deck"
+// which is a slice of strings
 type deck []string 
 
 func newDeck() deck {
@@ -32,6 +38,34 @@ func deal(d deck, handSize int) (deck, deck){
 	return d[:handSize], d[handSize:]
 }
 
-func (d deck) toString() string { // declaration functin, atribuitin function property to deck, function name, return type of function 
+func (d deck) toString() string { // declaration function, atribuitin function property to deck, function name, return type of function 
 	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck{
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Option #1 - Log the error and return a call to newDeck()
+		// Option #1 - Log the error and entirely quit the program
+		fmt.Println("Error: ", err)
+		os.Exit((1))
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d)-1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
